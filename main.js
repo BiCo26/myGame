ea4e18a1a5b781fe0315dbd42b3d3337fe32063d
BiCo26 <bianca.cortes26@gmail.com>
2017-07-13 14:33:42 -0400

    Space Games skeleton

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log('this $41T works!');
+
+
+
+
+	// var myWeapons=[""]; //declare the empty array - that will be loaded via the while loop below
+    // var slideIndex= 1;
+    // var p=0;
+	
+	// autoslide();
+
+
+    // //EXECUTES ONCE to load entire array
+    // while (p<5){ // 5 is the number of pictures you need to display from your folder
+    //     myWeapons[p]=`assets/weapon${p+1}.gif`; // loads the array. "finepix" is the id for where the pix load from
+    //     p++;
+    // }
+
+	// function autoslide() {
+	// 	var a = setInterval (plusDivs, 1);  //"a" is local variable / setInterval- calls the function plusDivs , sets the auto - to 3 seconds
+	// }
+
+	// 	function plusDivs() { //counter for next pic
+	// 		slideIndex+=1;//increments display
+	// 		showDivs(slideIndex);//calls slideIndex function
+	// 	}
+
+	// 	function showDivs(n){ 
+
+
+    //     if (slideIndex >= myWeapons.length) { //chex if its the last pic 2 go 2 1st pic again       CLEAR INTERVAL?
+    //         slideIndex= 0; 
+    //     }    
+
+    //     if (slideIndex < 0) {
+    //         slideIndex= myWeapons.length
+    //     }
+	// 	//retrieves pix from array ^^ && assigns 2 img tag to DISPLAY
+    //     document.getElementById("finepix").src = myWeapons[slideIndex]; 
+   
+	// 	}
