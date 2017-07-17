18c73a9b97f4e285a3e0b02f525ab0e8dda13a25
BiCo26 <bianca.cortes26@gmail.com>
2017-07-17 01:30:26 -0400

    Final Product for presentation

---
+++ ReadMe.md
@@ -0,0 +1,35 @@
+# Space Games
+Is a type of rock, paper, scissors game.
+
+
+
+## What is Space Games?
+  This space themed rock, paper, scissors game using comet, nebula, rocket instead
+
+## Technical Discussion
+
+> List of the technologies you're using (HTML, CSS, JavaScript)
+Space Games was created using HTML,CSS, and JavaScript.
+
+
+### Notes on Game Structure
+
+> Code samples, description of challenges you overcame, etc.
+A huge challenge for me was figuring out  a bug I created with the Math.random() function and setInterval.  I learned that the setInterval function when set at 100th of a second wrapped set to iterate 3 giphys 25 times = 2.5 seconds and so the winning prompt would have to be set above 2.5 seconds bc it would iterate many wins and many win/lose prompts instead of just 1 win/loss with 1 win / loss prompt. 
+
+## The Making of Capture The Flag
+
+> Any credits or notes you feel you should add
+ I brain stormed the idea of the game with Mili Neykova which also helped me with Math.Random() function.  Tom Santos helped with a few CSS issues.  Aurey Rodriguez helped with my setInterval issue and using innerText when localStorage nor innerHTML were working for my score keeping.
+  
+
+## Opportunities for Future Growth
+
+> If you had more time to work on your game, what would you do?
+With more time I would add more CSS pizazz , hover effects, animation and sound clips. I would also add a landnig page and I would change some of my prompts to be modals.  In the future I would also have the score store via localStorage.
+
+References:
+setTimeout() - https://www.w3schools.com/jsref/met_win_settimeout.asp 
+SetInterval() - 
+https://www.w3schools.com/jsref/met_win_setinterval.asp
+Math.random() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
--- main.js
+++ main.js
@@ -1,143 +1,145 @@
-console.log('this $41T works!');
+
+var userSelection = 0;
+var pcSelection = 0;
+var myWeapon=['./assets/weapon1.gif','./assets/weapon2.gif','./assets/weapon3.gif']; //declare the empty array - that will be loaded via the while loop below
+var slideIndex= 1;
+var p=0;
+var userWin = false;
+var pcWin = false;
+var userScore = 0;
+var pcScore = 0;
+var rock = 0;
+var paper = 1;
+var scissors = 2;
 
 
-    
-    var userSelection = '';
-    var pcSelection = Math.random();
-	var myWeapon=['./assets/weapon1.gif','./assets/weapon2.gif','./assets/weapon3.gif']; //declare the empty array - that will be loaded via the while loop below
-    var slideIndex= 1;
-    var p=0;
-	alert(myWeapon);
-
-
-window.onload = function() { //append to DISPLAY > load buttons
-//5
-//PLACEMENT? below buttons?
-
-    if ( userSelection == myWeapon[0]) {
-       
-        display = document.getElementById('w1'); //APPEND >DISPLAY
-        userSelection[0].append(display);
-         //rock
-    
-    }else if (userSelection == myWeapon[1]) {
-        display = document.getElementById('w2');
-        userSelection[1].append(display);
-
-    } else if (userSelection == myWeapon[2]){
-        display = document.getElementById('w3');
-        userSelection[2].append(display);
-        
-    }
-
-    //PLACEMENT?
-    //retrieves pix from array ^^ && assigns 2 img tag to DISPLAY
-        
-
-    //1
+window.onload = function() { // load buttons
 
     document.getElementById('w1').addEventListener('click', function(){//call setInterval func() here
-     userSelection =0; alert(1);
-     document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[userSelection]}')`
+     userSelection =rock; 
+     //pc display func()
+     slotMachine();
     });
 
     document.getElementById('w2').addEventListener('click', function(){
-    userSelection = 1; alert(2);
-    document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[userSelection]}')`
+    userSelection = paper; slotMachine();
     });
 
     document.getElementById('w3').addEventListener('click', function(){
-     userSelection = 2; alert("");
-    // document.getElementById('userDisplay').style["background-image"]=`url('assets/weapon2.gif')`
-    document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[userSelection]}')`
-    //  #userDisplay.background=myWeapon[userSelection]
+     userSelection = scissors; slotMachine();
+   
 
     });
 }//close onload
-
-//2
     
     
+//countdown SLIDESHOW  &&randomizer
 
+function slotMachine() { //counter for next pic
+    let image0;
+    let image1;
+    var slideIndex=0;
+    var setInt = setInterval (showWeapons, 100);  //setInterval- calls the function showWeapons & sets the auto - to 1 seconds
 
-		function slotMachine() { //counter for next pic
+    function showWeapons(){ 
 
-			var slideIndex=0;
-            
-            var setInt = setInterval (showDivs, 300);  //setInterval- calls the function showWeapons & sets the auto - to 3 seconds
-	
-            function showWeapons(){ 
-
-                let image0 = Math.floor(Math.random()*3);
-                let image = Math.floor(Math.random()*3);
-                if(slideIndex === 15 ){
-                    document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[image]}')`
-                }
-                // myWeapons
-               
-                // slideIndex++;
-            
-            }
+    
+        if(slideIndex <= 25 ){
+            image0 = Math.floor(Math.random()*3); //left display
+            image1 = Math.floor(Math.random()*3); //right display
+            pcSelection=image1;
+            document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[image0]}')`
+            document.getElementById('pcDisplay').style["background-image"]=`url('${myWeapon[image1]}')`
+            slideIndex++;
         }
-
-    //Arra y : 0=rock ; 1=paper; 2=scissors
-//3
-
-var rock = userSelection[0];
-var paper = [1];
-var scissors = [2];
-
-if (userSelection == rock && pcSelection == rock){
-    win != true;
-    alert("TIE!!!")
-}
-else if (userSelection == paper && pcSelection == paper){
-    win != true;
-    alert("TIE!!!")
-}
-else if (userSelection == scissors && pcSelection == scissors){
-    win != true;
-    alert("TIE!!!")
-}
-else if (userSelection == paper && pcSelection == paper){
-    win != true;
-    alert("TIE!!!")
-}
-//USER possibilities
-else if (userSelection == rock && pcSelection == paper){
-    win != false;
-    alert("You Lost!!!")
-}
-else if (userSelection == rock && pcSelection == scissors){
-    win != true;
-    alert("You Win!!!")
-}
-else if (userSelection == paper && pcSelection == rock){
-    win != true;
-    alert("TIE!!!")
+        else {
+            clearInterval(setInt);
+            document.getElementById('userDisplay').style["background-image"]=`url('${myWeapon[userSelection]}')`;            
+            
+            setTimeout(checkWinner,3000)  
+        }
+    
 }
 
-//PC IF STATEMENT to 'validate' randomizer
+}
 
+//Arra y : 0=rock ; 1=paper; 2=scissors
 
-//4
-//Math.random()???
+function checkWinner(){
 
-function pcRandomizer(myWeapon){
+    if (userSelection == pcSelection ){
+        alert('IT'S A TIE PLAY AGIAN!')
+    
+    
+    } else if (userSelection == rock && pcSelection == paper){
+        userWin = false;
+        pcWin = true;
+        confirm("YOU LOSE!!!")  
+    
+    } else if (userSelection == rock && pcSelection == scissors){
+        userWin = true;
+        pcWin = false;
+        confirm("YOU WIN!!!")
+    
+    } else if (userSelection == paper && pcSelection == rock){
+        userWin = true;
+        pcWin = false;
+        confirm("YOU WIN !!!")
+    
+    } else if (userSelection == paper && pcSelection == scissors){
+        userWin = false;
+        pcWin = true;
+        confirm("YOU LOSE !!!")
+    
+    } else if (userSelection == scissors && pcSelection == rock){
+        userWin = false;
+        pcWin = true;
+        confirm("YOU LOSE !!!")
+    
+    } else if (userSelection == scissors && pcSelection == paper){
+        userWin = false;
+        pcWin = true;
+        confirm("YOU WIN !!!")
+
+    }
+    
+    //SCORE looged via innerText to score div
+
+    if (userWin ==true && userWin == false) {
+        //future usage: localStorage.setItem('userScore',userScore); //add USERS win to local storage
+        userScore++;
         
-       myWeapon.forEach(
-            function(elem,i,myWeapon){
-
-                var newIndex= Math.floor(Math.random()* myWeapon.length);
-                var elem2=myWeapon[newIndex];
-                myWeapon[newIndex]=elem;
-                myWeapon[i]=elem2;
-            }
-        );
-        console.log(myWeapon);
-
-       return myWeapon;
+        document.getElementById('userScore').innerText= (userScore);
+    }
+    if (pcWin == true && userWin == false){
+         //localStorage.setItem('pcScore',pcScore); //add USERS win to local storage
+        pcScore++;
+        document.getElementById('pcScore').innerText= (pcScore);
     }
 
+////FIRST TO 3 WINS
 
-pcRandomizer([1,0,3])
+   if (userScore == 3){
+       alert("YOU WON THE MATCH!")
+    }
+   if (pcScore == 3){
+       alert("You Lost the Match")
+    }
+   
+
+    if(confirm('PLAY AGAIN!') == true) {
+        document.getElementById('userDisplay').style["background-image"]=`url('./assets/display.gif')`
+        document.getElementById('pcDisplay').style["background-image"]=`url('./assets/display.gif')`
+        
+        userSelection = 0;
+        pcSelection = 0;
+        slideIndex= 1;
+        p=0;
+        userWin = false;
+        pcWin = false;
+        
+        
+    }
+}
+
+
